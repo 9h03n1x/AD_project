fb3b80bb647539c322b3d2cece3953f817e63adc
9h03n1x <nicco.dillmann@freenet.de>
2019-03-10 15:21:05 +0100

    added a behavior planner in the plan modul

---
+++ src/plan/behavior_planner.py
@@ -0,0 +1,17 @@
+'''
+Created on 10.03.2019
+
+@author: Nicco
+'''
+
+class behavior_planner(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
